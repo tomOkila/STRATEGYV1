047c5b38148f9a027950de6b95ed74f539c65600
TOM OKILA <thomasokilla@gmail.com>
2025-10-02 16:41:26 +0300

    02.10.2025

--- STRATEGY.CLIENT/Models/Permissions.cs
+++ STRATEGY.CLIENT/Models/Permissions.cs
@@ -7,6 +7,9 @@ namespace STRATEGY.CLIENT.Models
         [Key]
         public int PermissionId { get; set; }
         public string PermissionName { get; set; } = string.Empty;
+        public int Create { get; set; }
+        public int Update { get; set; }
+        public int Delete { get; set; }
         public DateTime CreateDate { get; set; }
         public int UpdatedBy { get; set; }
         public DateTime UpdatedDate { get; set; }
---
+++ STRATEGY.WEBAPI/Migrations/20251002132826_AddPermissionFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace STRATEGY.WEBAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermissionFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Create",
+                table: "Permissions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Delete",
+                table: "Permissions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Update",
+                table: "Permissions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Create",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "Delete",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "Update",
+                table: "Permissions");
+        }
+    }
+}
