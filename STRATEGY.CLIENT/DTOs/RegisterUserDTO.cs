a63e0a8aea8203334d31cb669b4243f8fe462549
TOM OKILA <thomasokilla@gmail.com>
2025-11-04 17:40:06 +0300

    04112025

--- STRATEGY.CLIENT/DTOs/RegisterUserDTO.cs
+++ STRATEGY.CLIENT/DTOs/RegisterUserDTO.cs
@@ -9,6 +9,7 @@ namespace STRATEGY.CLIENT.DTOs
         public string Name { get; set; } = string.Empty;
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+
         [Required]
         public string Password { get; set; } = string.Empty;
 
